3de5d2b2ffff146e9510a2a0606b3b3c6e15c4ad
chenglan28 <76613821+chenglan28@users.noreply.github.com>
2020-12-26 19:10:59 -0600

    Add files via upload

---
+++ bilibili/main.js
@@ -0,0 +1,8 @@
+function bilibili(){
+document.getElementById("tag").value = "command";
+document.getElementById("input").value = "open bilibili";
+start();
+}
+function close_bilibili(){
+document.getElementById("bilibili").style.display= "none";
+}
