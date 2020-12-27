543b73a33efefbbe018cd78e5b0ca8ecd5a81548
chenglan28 <76613821+chenglan28@users.noreply.github.com>
2020-12-26 19:04:48 -0600

    Add files via upload

---
+++ baidu/main.js
@@ -0,0 +1,11 @@
+function baidu(){
+document.getElementById("tag").value = "command";
+document.getElementById("input").value = "open baidu";
+start();
+}
+function close_tag_baidu(){
+document.getElementById("tag_baidu").style.display= "none";
+}
+function close_baidu(){
+document.getElementById("baidu").style.display= "none";
+}
